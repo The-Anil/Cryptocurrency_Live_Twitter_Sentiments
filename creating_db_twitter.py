3938f3cab3eab2686d73209d3a8921712e8fa795
Anil Kumar Yadav <anilyadv2014@gmail.com>
2018-04-19 03:54:23 +0530

    Cryptocurrency_Live_Twitter_Sentiments
    Python project analysing twitter feed and plotting sentiments against time. Used Dash for better plotting and visualization in webApp.

---
+++ creating_db_twitter.py
@@ -0,0 +1,67 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+import sqlite3
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from unidecode import unidecode
+import time, datetime
+
+analyzer = SentimentIntensityAnalyzer()
+
+#consumer key, consumer secret, access token, access secret.
+ckey="JvUsmX3UfzBDNuajlI052eMdy"
+csecret="9tVK36bNLwJ4x2e49ZrkhLW4Lb6Fdk7rE8jMviK6B0h2KOSdQt"
+atoken="789686945047773184-6TSTGgnx33sGIndbkxMgTfMYbl40L35"
+asecret="svOZ4DzhE0TlZaFw93pAnXiNlhZsKJ1YuYuzlOzz50a0k"
+
+conn = sqlite3.connect('twitter.db')
+c = conn.cursor()
+
+def create_table():
+    try:
+        c.execute("CREATE TABLE IF NOT EXISTS sentiment(unix REAL, tweet TEXT, sentiment REAL)")
+        c.execute("CREATE INDEX fast_unix ON sentiment(unix)")
+        c.execute("CREATE INDEX fast_tweet ON sentiment(tweet)")
+        c.execute("CREATE INDEX fast_sentiment ON sentiment(sentiment)")
+        conn.commit()
+    except Exception as e:
+        print(str(e))
+create_table()
+
+
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        try:
+            data = json.loads(data)
+            tweet = unidecode(data['text'])
+            time_ms = data['timestamp_ms']
+            vs = analyzer.polarity_scores(tweet)
+            sentiment = vs['compound']
+            c.execute("INSERT INTO sentiment (unix, tweet, sentiment) VALUES (?, ?, ?)",
+                  (time_ms, tweet, sentiment))
+            your_dt = datetime.datetime.fromtimestamp(int(time_ms)/1000)
+            #time.strftime("%D %H:%M", time.localtime(int(time_ms.strip()[0:9])))
+            print(your_dt.strftime("%Y-%m-%d %H:%M:%S"), tweet, sentiment)
+            conn.commit()
+
+        except KeyError as e:
+            print(str(e))
+        return(True)
+
+    def on_error(self, status):
+        print(status)
+
+
+while True:
+
+    try:
+        auth = OAuthHandler(ckey, csecret)
+        auth.set_access_token(atoken, asecret)
+        twitterStream = Stream(auth, listener())
+        twitterStream.filter(track=["bitcoin","ethereum","monero","litecoin","ripple","steem","maidsafe","iota"])
+    except Exception as e:
+        print(str(e))
+        time.sleep(5)
